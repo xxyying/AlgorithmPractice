c3c711a91c3c09df0ef0b51e41e3e77da5702739
Yiying Liu <ceci.liuyiying@outlook.com>
2017-06-26 15:07:28 -0700

    - updated 028

--- src/com/xxyying/algorithm/L028Strstr.java
+++ src/com/xxyying/algorithm/L028Strstr.java
@@ -5,6 +5,7 @@ public class L028Strstr {
 		if (haystack == null || needle == null || haystack.length() < needle.length()) {
         	return -1;
         }
+		if(needle.length()==haystack.length()) return needle.equals(haystack) ?  0:-1;
 		for (int i = 0; i < haystack.length() - needle.length() + 1; i++) {
 			int cnt = 0;
 			while(cnt < needle.length() && haystack.charAt(i + cnt) == needle.charAt(cnt)) {
