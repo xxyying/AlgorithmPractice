5338075723d8b606f55458f45c1ec9428bb7efa5
Yiying Liu <ceci.liuyiying@outlook.com>
2017-06-26 13:51:44 -0700

    - update

---
+++ src/com/xxyying/algorithm/L028Strstr.java
@@ -0,0 +1,12 @@
+package com.xxyying.algorithm;
+
+public class L028Strstr {
+	public int strStr(String haystack, String needle) {
+        
+    }
+	public static void main(String[] args) {
+		L028Strstr test = new L028Strstr();
+		
+	}
+
+}
